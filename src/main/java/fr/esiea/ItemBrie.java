4bbfa775793783c0938c1fa622622e54805b7f81
AminRas <35491292+AminRas@users.noreply.github.com>
2018-02-28 23:46:02 +0100

    Update ItemBrie.java

--- src/main/java/fr/esiea/ItemBrie.java
+++ src/main/java/fr/esiea/ItemBrie.java
@@ -6,10 +6,21 @@ public class ItemBrie extends Item {
     //**************************************************************************
     //   CONSTRUCTOR
     //**************************************************************************
+    /**
+     * Constructor of Brie with arguments
+     * @param name
+     * @param sellIn
+     * @param quality
+     * @param qte
+     * @param price
+     */
     public ItemBrie(String name, int sellIn, int quality, int qte, double price) {
         super(name, sellIn, quality, qte, price);
     }
 
+    /**
+     * Constructor of Brie without arguments, all is set by default.
+     */
     public ItemBrie() {
         super("Aged Brie",10, 10, 10, 9.99);
     }
@@ -18,6 +29,10 @@ public class ItemBrie extends Item {
     //**************************************************************************
     //   METHODS
     //**************************************************************************
+
+    /**
+     * Update method that override the mother one with a specific behavior
+     */
     @Override
     public void update() {
         increaseQualitySafely();
