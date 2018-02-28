74a0dc48702c63cd0fc5a74c81e1a3c77e638fe1
AminRas <35491292+AminRas@users.noreply.github.com>
2018-02-28 23:44:48 +0100

    Update ItemBackstagePass.java

--- src/main/java/fr/esiea/ItemBackstagePass.java
+++ src/main/java/fr/esiea/ItemBackstagePass.java
@@ -6,10 +6,22 @@ public class ItemBackstagePass extends Item {
     //**************************************************************************
     //   CONSTRUCTOR
     //**************************************************************************
+
+    /**
+     * Constructor of BackstagePass with arguments
+     * @param name
+     * @param sellIn
+     * @param quality
+     * @param qte
+     * @param price
+     */
     public ItemBackstagePass(String name, int sellIn, int quality, int qte, double price) {
         super(name, sellIn, quality, qte, price);
     }
 
+    /**
+     * Constructor of BackstagePass without arguments, all is set by default.
+     */
     public ItemBackstagePass() {
         super("Backstage passes to a TAFKAL80ETC concert",10, 10, 10, 40.99);
     }
@@ -18,6 +30,10 @@ public class ItemBackstagePass extends Item {
     //**************************************************************************
     //   METHODS
     //**************************************************************************
+
+    /**
+     * Update method that override the mother one with a specific behavior
+     */
     @Override
     public void update() {
         this.increaseQuality();
