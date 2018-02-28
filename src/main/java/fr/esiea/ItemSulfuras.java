89c9e9f5eb79b1ba85e686d378f4a592ad5041ab
AminRas <35491292+AminRas@users.noreply.github.com>
2018-02-28 23:48:06 +0100

    Update ItemSulfuras.java

--- src/main/java/fr/esiea/ItemSulfuras.java
+++ src/main/java/fr/esiea/ItemSulfuras.java
@@ -6,10 +6,22 @@ public class ItemSulfuras extends Item {
     //**************************************************************************
     //   CONSTRUCTOR
     //**************************************************************************
+
+    /**
+     * Constructor of Sulfuras with arguments
+     * @param name
+     * @param sellIn
+     * @param quality
+     * @param qte
+     * @param price
+     */
     public ItemSulfuras(String name, int sellIn, int quality, int qte, double price) {
         super(name, sellIn, quality, qte, price);
     }
 
+    /**
+     * Constructor of Sulfuras without arguments
+     */
     public ItemSulfuras() {
         super("Sulfuras, Hand of Ragnaros", 20, 50, 1, 1000000.99);
     }
@@ -18,6 +30,11 @@ public class ItemSulfuras extends Item {
     //**************************************************************************
     //   METHODS
     //**************************************************************************
+
+    /**
+     * Update method that override the mother one with a specific behavior,
+     * notice that as Sulfuras is legendary item, nothing affect it.
+     */
     @Override
     public void update() {
 
